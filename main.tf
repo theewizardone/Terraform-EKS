d8dbec76a6a49ea35d249c064a4636ed93871d3c
Alfonce Morara <alfoncemorara412@gmail.com>
2025-05-20 10:47:14 +0300

    updated main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,9 @@ module "vpc" {
 
   azs                     = data.aws_availability_zones.azs.names
   public_subnets          = var.public_subnets
-  map_public_ip_on_launch = enable_dns_hostnames = true
+  map_public_ip_on_launch = true
+  enable_dns_hostnames    = true
+
 
   tags = {
     name        = "jenkins-vpc"
