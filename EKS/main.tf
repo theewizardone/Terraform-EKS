b382a941ad96224b05b5ee3ac2a279b59a032f52
Alfonce Morara <alfoncemorara412@gmail.com>
2025-05-21 18:26:11 +0300

    Adding files to repo

---
+++ EKS/main.tf
@@ -0,0 +1,58 @@
+# VPC Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name            = "jenkins-vpc"
+  cidr            = var.vpc_cidr
+  azs             = data.aws_availability_zones.azs.names
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/elb"               = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/role/internal-elb"      = "1"
+  }
+}
+
+# EKS Module
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.31"
+
+
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+
+  eks_managed_node_groups = {
+    nodes = {
+      instance_types = ["t2.small"]
+      min_size       = 1
+      max_size       = 5
+      desired_size   = 2
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
---
+++ EKS/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+
+}
+
+variable "private_subnets" {
+  description = " subnets CIDR"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = " subnets CIDR"
+  type        = list(string)
+}
